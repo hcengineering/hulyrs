fb734b2ed7f73c0e449384b0d5cb8dbf33ffa59f
Leonid Kaganov <lleo@lleo.me>
2025-08-04 17:35:32 +0300

    Fix rand 0.9.1 to 0.9 for compatibility

---
+++ commmit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+clear
+
+#git push origin --delete feature/insert_update
+#git push origin --delete feature/insert_update_uuid
+
+NAME="fix rang"
+
+git checkout -b feature/${NAME}
+git add .
+git commit -m "Fix rand 0.9.1 to 0.9 for compatibility"
+git push origin feature/${NAME}
