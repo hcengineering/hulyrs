6c35c4aeec69909a3da2ea3234f64c1b3f3a24ae
Alex <69764315+Serial-ATA@users.noreply.github.com>
2025-08-28 22:01:03 -0400

    feat: support lookups (#18)

--- src/services/core/storage.rs
+++ src/services/core/storage.rs
@@ -1,5 +1,54 @@
-use super::classes::OperationDomain;
+use crate::services::core::classes::OperationDomain;
+use crate::services::event::{Class, HasId};
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::collections::HashMap;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Source {
+    #[serde(rename = "$score")]
+    pub score: f64,
+    #[serde(flatten)]
+    pub extra: HashMap<String, Value>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct WithLookup<T> {
+    #[serde(flatten)]
+    pub doc: T,
+
+    #[serde(rename = "$lookup", skip_serializing_if = "Option::is_none")]
+    pub lookup: Option<HashMap<String, Value>>,
+
+    #[serde(rename = "$associations", skip_serializing_if = "Option::is_none")]
+    pub associations: Option<HashMap<String, Vec<Value>>>,
+
+    #[serde(rename = "$source", skip_serializing_if = "Option::is_none")]
+    pub source: Option<Source>,
+}
+
+impl<C: Class> Class for WithLookup<C> {
+    const CLASS: &'static str = C::CLASS;
+}
+
+impl<C: HasId> HasId for WithLookup<C> {
+    fn id(&self) -> &str {
+        self.doc.id()
+    }
+}
+
+impl<C: PartialEq> PartialEq for WithLookup<C> {
+    fn eq(&self, other: &Self) -> bool {
+        self.doc.eq(&other.doc)
+    }
+}
+
+impl<T> WithLookup<T> {
+    pub fn into_inner(self) -> T {
+        self.doc
+    }
+}
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
--- src/services/transactor/document.rs
+++ src/services/transactor/document.rs
@@ -166,7 +166,94 @@ impl Transaction for RemoveDocument {
     }
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(untagged)]
+pub enum LookupValue {
+    Simple(String),
+    Nested(String, Box<Lookup>),
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(untagged)]
+pub enum ReverseLookupValue {
+    Simple(String),
+    WithAttribute(String, String),
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+pub struct Lookup {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    pub reverse_lookups: Option<HashMap<String, ReverseLookupValue>>,
+
+    #[serde(flatten)]
+    pub forward_lookups: HashMap<String, LookupValue>,
+}
+
+#[macro_export]
+macro_rules! lookup_reverse {
+    (@build $map:expr,) => {};
+
+    (@build $map:expr, $key:ident: $value:expr, $($rest:tt)*) => {
+        $map.insert(
+            stringify!($key).to_string(),
+            $crate::services::transactor::document::ReverseLookupValue::Simple($value.into()),
+        );
+        $crate::lookup_reverse!(@build $map, $($rest)*);
+    };
+
+    (@build $map:expr, $key:ident: [$class:expr, $attr:expr], $($rest:tt)*) => {
+        $map.insert(
+            stringify!($key).to_string(),
+            $crate::services::transactor::document::ReverseLookupValue::WithAttribute($class.into(), $attr.into()),
+        );
+        $crate::lookup_reverse!(@build $map, $($rest)*);
+    };
+}
+
+#[macro_export]
+macro_rules! lookup {
+    // Base case
+    (@build $map:expr, $(,)*) => {};
+
+    (@build $lookup:expr, _id: { $($reverse_tts:tt)* }, $($rest:tt)*) => {
+        {
+            let mut reverse_map = ::std::collections::HashMap::new();
+            $crate::lookup_reverse!(@build &mut reverse_map, $($reverse_tts)*,);
+            $lookup.reverse_lookups = Some(reverse_map);
+        }
+        $crate::lookup!(@build $lookup, $($rest)*);
+    };
+
+    (@build $lookup:expr, $key:ident: $value:expr, $($rest:tt)*) => {
+        $lookup.forward_lookups.insert(
+            stringify!($key).to_string(),
+            $crate::services::transactor::document::LookupValue::Simple($value.into()),
+        );
+        $crate::lookup!(@build $lookup, $($rest)*);
+    };
+
+    (@build $lookup:expr, $key:ident: [$value:expr, { $($nested_tts:tt)* }], $($rest:tt)*) => {
+        $lookup.forward_lookups.insert(
+            stringify!($key).to_string(),
+            $crate::services::transactor::document::LookupValue::Nested(
+                $value.into(),
+                Box::new($crate::lookup!{ $($nested_tts)* }),
+            ),
+        );
+        $crate::lookup!(@build $lookup, $($rest)*);
+    };
+
+    ( $($tts:tt)* ) => {
+        {
+            let mut lookup = $crate::services::transactor::document::Lookup::default();
+            $crate::lookup!(@build &mut lookup, $($tts)*,);
+            lookup
+        }
+    };
+}
+
 #[derive(Serialize, Deserialize, Debug, Default, Clone, Builder)]
+#[builder(build_fn(private, name = "fallible_build"))]
 #[serde(rename_all = "camelCase")]
 pub struct FindOptions {
     #[builder(setter(strip_option), default)]
@@ -174,7 +261,9 @@ pub struct FindOptions {
     limit: Option<u32>,
 
     // sort?: SortingQuery<T>
-    // lookup?: Lookup<T>
+    #[builder(setter(strip_option), default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub lookup: Option<Lookup>,
     // projection?: Projection<T>
     // associations?: AssociationQuery[]
     #[builder(setter(custom), default)]
@@ -186,6 +275,12 @@ pub struct FindOptions {
 
     #[builder(default)]
     show_archived: bool,
+}
+
+impl FindOptions {
+    pub fn builder() -> FindOptionsBuilder {
+        FindOptionsBuilder::default()
+    }
 }
 
 impl FindOptionsBuilder {
@@ -200,6 +295,11 @@ impl FindOptionsBuilder {
             .insert(field.to_owned(), 1);
 
         self
+    }
+
+    pub fn build(&mut self) -> FindOptions {
+        self.fallible_build()
+            .expect("All required fields set at initialization")
     }
 }
 
@@ -251,23 +351,27 @@ impl<B: Backend> DocumentClient for super::TransactorClient<B> {
             )
             .await?;
 
-        // TODO?
-        /* api-client/src/rest.ts
-        if (result.lookupMap !== undefined) {
-            // We need to extract lookup map to document lookups
-            for (const d of result) {
-              if (d.$lookup !== undefined) {
-                for (const [k, v] of Object.entries(d.$lookup)) {
-                  if (!Array.isArray(v)) {
-                    d.$lookup[k] = result.lookupMap[v as any]
-                  } else {
-                    d.$lookup[k] = v.map((it) => result.lookupMap?.[it])
-                  }
+        // as in api-client/src/rest.ts
+        if let Some(lookup_map) = &result.lookup_map {
+            for entry in result.value.iter_mut() {
+                let Some(obj_lookup) = entry.get_mut("$lookup").and_then(Value::as_object_mut)
+                else {
+                    continue;
+                };
+
+                for value in obj_lookup.values_mut() {
+                    if let Some(array) = value.as_array_mut() {
+                        for item in array {
+                            if let Some(lookup_key) = item.as_str() {
+                                *item = lookup_map.get(lookup_key).cloned().unwrap_or(Value::Null)
+                            }
+                        }
+                    } else if let Some(lookup_key) = value.as_str() {
+                        *value = lookup_map.get(lookup_key).cloned().unwrap_or(Value::Null)
+                    }
                 }
-              }
             }
-            delete result.lookupMap
-          }*/
+        }
 
         // as in api-client/src/rest.ts
         for entry in result.value.iter_mut() {
